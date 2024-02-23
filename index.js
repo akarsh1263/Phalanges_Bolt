38f9dec5ece50f92cb96c0aba73e6f94ebc2c05c
akarsh1263 <vyoopsoftwares@gmail.com>
2024-02-23 14:07:18 +0530

    firebase for video call

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const express=require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const firebase = require('firebase/app');
+require('firebase/firestore');
+
 const connectToMongoDB = require('./db.js');
 require('dotenv').config();
 const cors = require('cors');
@@ -7,6 +12,9 @@ const patientRoutes = require('./routes/patient_routes.js')
 const doctorRoutes = require('./routes/doctor_routes.js')
 
 const app=express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
 app.use(express.json());
 app.use(cors({ origin: '*', }));
 app.use(bodyParser.json());
@@ -23,6 +31,49 @@ async function connectDB() {
 
 connectDB();
 
+// video call backend with firebase:
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDUPcw2b-ZvKl0yrdCFM7l2OkxKOIyoTXI",
+  authDomain: "phalanges-bolt.firebaseapp.com",
+  projectId: "phalanges-bolt",
+  storageBucket: "phalanges-bolt.appspot.com",
+  messagingSenderId: "812479468092",
+  appId: "1:812479468092:web:fd88cec4875e30e4b2a939",
+  measurementId: "G-TM5Y0EB875"
+  };
+  
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  }
+  const firestore = firebase.firestore();
+
+  io.on('connection', (socket) => {
+    socket.on('webcamOffer', async (offer) => {
+      const callDoc = firestore.collection('calls').doc();
+      const offerCandidates = callDoc.collection('offerCandidates');
+      const answerCandidates = callDoc.collection('answerCandidates');
+  
+      await offerCandidates.add(offer);
+
+      callDoc.onSnapshot(async (snapshot) => {
+        const data = snapshot.data();
+        if (!data?.answer) return;
+
+        socket.emit('webcamAnswer', data.answer);
+      });
+
+      socket.on('webcamCandidate', (candidate) => {
+        answerCandidates.add(candidate);
+      });
+    });
+  
+    socket.on('disconnect', () => {
+      cconsole.log('disconnected')
+    });
+  });
+  
+  
 app.use('/patient', patientRoutes);
 app.use('/doctor', doctorRoutes)
 
